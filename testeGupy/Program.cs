26b55a395e3f706158e792bea89b80e052a2849c
Daniel <danielcarmo2@gmail.com>
2024-09-04 16:31:38 -0300

    m

---
+++ testeGupy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número a ser verificado: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        while (b < numero)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        if (b == numero || numero == 0)
+        {
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+        }
+
+
+
+
+        Console.Write("Informe uma string: ");
+        string input = Console.ReadLine();
+
+        int countLowerA = 0;
+        int countUpperA = 0;
+
+        foreach (char c in input)
+        {
+            if (c == 'a')
+            {
+                countLowerA++;
+            }
+            else if (c == 'A')
+            {
+                countUpperA++;
+            }
+        }
+
+        int totalA = countLowerA + countUpperA;
+
+        if (totalA > 0)
+        {
+            Console.WriteLine($"A letra 'a' (maiúscula ou minúscula) aparece {totalA} vezes na string.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' (maiúscula ou minúscula) não aparece na string.");
+        }
+    }
+}
